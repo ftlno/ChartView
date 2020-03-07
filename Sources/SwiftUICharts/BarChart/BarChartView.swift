53c1398c6fbc91f6d6edfa9b2fdd9d0197c47c79
Fredrik Thorbjørnsen Lillejordet <fretholil@gmail.com>
2020-03-07 08:40:38 +0100

    int double

--- Sources/SwiftUICharts/BarChart/BarChartView.swift
+++ Sources/SwiftUICharts/BarChart/BarChartView.swift
@@ -59,7 +59,7 @@ public struct BarChartView: View {
                             .font(.headline)
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                     }else{
-                        Text("\(self.currentValue, specifier: self.valueSpecifier)")
+                        Text("\(Int(self.currentValue), specifier: self.valueSpecifier)")
                             .font(.headline)
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
                     }
